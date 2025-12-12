87355d118ef491430220292d6855ac6422562e49
Niks <ktm.softwear@gmail.com>
2025-12-12 15:11:12 +0300

    Fix  paths

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
-} from 'react-router-dom'
+} from "react-router-dom";
 
-import './style.css'
-import ContactsAndRegistration from './views/contacts-and-registration'
-import Home from './views/home'
-import DisciplinesClasses from './views/disciplines-classes'
-import AboutUs from './views/about-us'
-import NotFound from './views/not-found'
+import "./style.css";
+import ContactsAndRegistration from "./views/contacts-and-registration";
+import Home from "./views/home";
+import DisciplinesClasses from "./views/disciplines-classes";
+import AboutUs from "./views/about-us";
+import NotFound from "./views/not-found";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Switch>
         <Route
           component={ContactsAndRegistration}
@@ -34,7 +34,7 @@ const App = () => {
         <Redirect to="**" />
       </Switch>
     </Router>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById("app"));
